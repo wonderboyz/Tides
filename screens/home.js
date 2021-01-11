44210f2ecc18e3d50e61f7653b8d05dfbbc1ae56
dang lu <dangnlu18@gmail.com>
2021-01-11 09:50:58 -0800

    charts

--- screens/home.js
+++ screens/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button, View, Text, StyleSheet} from 'react-native';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {LineChart} from 'react-native-chart-kit'
 
 
 const arr1 = ['hello', 'world']
@@ -41,10 +42,55 @@ function HomeScreen({navigation}){
             <View style={{alignItems:'center' }}><Ionicons name='sunny-outline' size={50} color={'white'}/><Text style={{color:'white', paddingTop: 10}}>72°F | 64°F</Text></View>
         </View>
 
+        <View style={{backgroundColor:'#40b9de'}}>
+            <LineChart 
+                data={{
+                    labels:['day1', 'day2', 'day3', 'day4'],
+                    datasets:[
+                        {
+                            data:[
+                                6.24,
+                                3,
+                                4,5,
+                                -0.22
+                            ]
+                        }
+                    ]
+                }}
+
+                width={500} // from react-native
+                height={250}
+                yAxisLabel="$"
+                yAxisSuffix="k"
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                  backgroundColor: "#e26a00",
+                  backgroundGradientFrom: "#fb8c00",
+                  backgroundGradientTo: "#ffa726",
+                  decimalPlaces: 2, // optional, defaults to 2dp
+                  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  style: {
+                    borderRadius: 6
+                  },
+                  propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#ffa726"
+                  }
+                }}
+                bezier
+                style={{
+                  marginVertical: 2,
+                  borderRadius: 8
+                }}
+            />
+        </View>
+{/* 
         <View style={{flex:2}}>{arr1.map(item => {
             return <Text>{item}</Text>
-        })}</View>
-        <View style={{flex:1, backgroundColor:'orange'}}><Text>Tide Info</Text></View>
+        })}</View> */}
+        <View style={{flex:1, backgroundColor:'#40b9de'}}><Text>Tide Info</Text></View>
     </View>
     )
 }
